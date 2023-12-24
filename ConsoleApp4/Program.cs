828e9dd81db36caebda8ec4837fa41bb9c1797da
rchudaeva <weather11ch@gmail.com>
2023-12-24 20:32:46 +0300

    3, 4 задачи

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,17 @@ namespace SecondHomeWork
     {
         static void Main(string[] args)
         {
-            Second1();
-            Console.Clear();
-            Second2();
-        
-        void Second1()
+            double k; 
+            Console.WriteLine("Введите номер задачи (от 1 до 6)");
+            k = Convert.ToDouble(Console.ReadLine());
+            if (k==1) Second1();
+            if (k==2) Second2();
+            if (k==3) Second3();
+            if (k==4) Second4();
+            //Console.Clear();
+            
+
+            void Second1()
         {
             double a; double b; double result;
             Console.WriteLine("Введите два числа");
@@ -54,6 +61,62 @@ namespace SecondHomeWork
             Console.ReadLine();
             return;
         }
-    }
+            void Second3()
+            {
+                double a; double b; double c; double result;
+                Console.WriteLine("Введите три числа");
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                c = Convert.ToDouble(Console.ReadLine());
+                if (a < b)
+                    if (b < c) Console.WriteLine(a + ", " + b + ", " + c);
+                    else if (a < c) Console.WriteLine(a + ", " + c + ", " + b);
+                         else Console.WriteLine(c + ", " + a + ", " + b);
+                else
+                    if (b > c) Console.WriteLine(c + ", " + b + ", " + a);
+                else
+                    if (c > a) Console.WriteLine(b + ", " + a + ", " + c);
+                else Console.WriteLine(b + ", " + c + ", " + a);
+
+
+                Console.ReadLine();
+            }
+
+            void Second4()
+            {
+                double a; double b; double c; double d; double x1; double x2;
+                Console.WriteLine("Введите три числа для решения квадратного уравнения");
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                c = Convert.ToDouble(Console.ReadLine());
+                d = b * b - 4 * a * c; Console.WriteLine("D = " + d);
+                    if (d > 0) { x1 = (-b + Math.Sqrt(d))/(2*a); x2 = (-b - Math.Sqrt(d))/(2*a);
+                    Console.WriteLine("x1 = " + x1 + "; x2 = " + x2); }
+                else
+                    if (d == 0) {x1 = -b / (2 * a);  Console.WriteLine("x = " + x1); }
+                else Console.WriteLine("решения нет");
+                
+                Console.ReadLine();
+            }
+            void Second5()
+            {
+                double a; double b; double c; double d; double x1; double x2;
+                Console.WriteLine("Введите три числа для решения квадратного уравнения");
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                c = Convert.ToDouble(Console.ReadLine());
+                d = b * b - 4 * a * c; Console.WriteLine("D = " + d);
+                if (d > 0)
+                {
+                    x1 = (-b + Math.Sqrt(d)) / (2 * a); x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    Console.WriteLine("x1 = " + x1 + "; x2 = " + x2);
+                }
+                else
+                if (d == 0) { x1 = -b / (2 * a); Console.WriteLine("x = " + x1); }
+                else Console.WriteLine("решения нет");
+
+                Console.ReadLine();
+            }
+        }
     }
 }
