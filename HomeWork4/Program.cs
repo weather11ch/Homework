1f8b5cf9571e5e0a20bd6b028f6a7f0e862e2144
rchudaeva <weather11ch@gmail.com>
2024-01-07 13:40:56 +0300

    test

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] createArray(int l)
+            {
+                int[] r = new int[l];
+                Random rnd = new Random();
+                for (int i = 0; i <= l; i++)
+                {
+                    r[i] = rnd.Next(l);
+                    Console.Write(r[i] + " ");
+                }
+                return r;
+            }
+            createArray(10);
+        }
+    }
+}
