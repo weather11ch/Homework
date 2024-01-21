ae2381e203d0a0492c1be135f33f3954c21139ff
rchudaeva <weather11ch@gmail.com>
2024-01-21 19:36:17 +0300

    исправила ошибки по одномерным масивам и добавила 9 и 10

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -11,14 +11,22 @@ namespace HomeWork4
     {
         static void Main(string[] args)
         {
-            //forth1();
-            //forth2();
-            //forth3()
-            //forth4();
-            //forth5();
-            //forth6();
-            //forth7();
-            forth8();
+            int k;
+            Console.WriteLine("Введите номер задачи (от 1 до 10)");
+            k = Int32.Parse(Console.ReadLine());
+            switch (k)
+            {
+                case 1: Forth1(); break;
+                case 2: Forth2(); break;
+                case 3: Forth3(); break;
+                case 4: Forth4(); break;
+                case 5: Forth5(); break;
+                case 6: Forth6(); break;
+                case 7: Forth7(); break;
+                case 8: Forth8(); break;
+                case 9: Forth9(); break;
+                case 10: Forth10(); break;                
+            }
             Console.ReadLine();
         }
         
@@ -40,7 +48,7 @@ namespace HomeWork4
                 Console.Write(r[i] + " ");
             }
         }
-        static void forth1()
+        static void Forth1()
         //1.	Найти минимальный элемент массива
         {
             Console.WriteLine("задайте длину одномерного массива");
@@ -50,16 +58,16 @@ namespace HomeWork4
 
             int[] r = createArray(l, n);
             
-            int min = r[0]; int minIndex = 0;
+            int minIndex = 0;
             for (int i = 1; i < l; i++)
             {
-                if (r[i] < min) { minIndex = i; }
+                if (r[i] < r[minIndex]) { minIndex = i; }
                 
             }                       
             Console.WriteLine("min = " + r[minIndex]);
             
         }
-        static void forth2()
+        static void Forth2()
             //2.	Найти максимальный элемент массива
         {
             Console.WriteLine("задайте длину одномерного массива");
@@ -68,15 +76,15 @@ namespace HomeWork4
             int n = Int32.Parse(Console.ReadLine());
 
             int[] r = createArray(l, n);
-            int max = r[0]; int maxIndex = 0;
+            int maxIndex = 0;
             for (int i = 1; i < l; i++)
             {                
-                if (r[i] > max) {maxIndex = i; }
+                if (r[i] > r[maxIndex]) {maxIndex = i; }
             }
 
             Console.WriteLine("max = " + r[maxIndex]);
         }
-        static void forth3()
+        static void Forth3()
         //3.	Найти индекс минимального элемента массива
         {
             Console.WriteLine("задайте длину одномерного массива");
@@ -86,16 +94,16 @@ namespace HomeWork4
 
             int[] r = createArray(l, n);
             
-            int min = r[0]; int minIndex = 0; 
+            int minIndex = 0; 
             for (int i = 1; i < l; i++)
             {
-                if (r[i] < min) {minIndex = i; }                
+                if (r[i] < r[minIndex]) {minIndex = i; }                
             }
 
             Console.WriteLine(" minIndex = " + minIndex);           
             
         }
-        static void forth4()
+        static void Forth4()
         //4.	Найти индекс максимального элемента массива
         {
             Console.WriteLine("задайте длину одномерного массива");
@@ -104,17 +112,17 @@ namespace HomeWork4
             int n = Int32.Parse(Console.ReadLine());
 
             int[] r = createArray(l, n);
-           
-            int max = r[0]; int maxIndex = 0;
+            Console.WriteLine();
+            int maxIndex = 0;
             for (int i = 1; i < l; i++)
             {               
-                if (r[i] > max) { maxIndex = i; }
+                if (r[i] > r[maxIndex]) { maxIndex = i; }
             }
 
             Console.WriteLine(" maxIndex = " + maxIndex);
         }
 
-        static void forth5()
+        static void Forth5()
         //5.	Посчитать сумму элементов массива с нечетными индексами
         {
             Console.WriteLine("задайте длину одномерного массива");
@@ -131,7 +139,7 @@ namespace HomeWork4
             }
             Console.WriteLine("сумма нечетных элементов массива (с нечетными индексами) равна " + sum);
         }
-        static void forth6()
+        static void Forth6()
         //6.	Сделать реверс массива (массив в обратном направлении)
         {
             Console.WriteLine("задайте длину одномерного массива");
@@ -153,7 +161,7 @@ namespace HomeWork4
             Console.WriteLine("перевернутый массив реверс");
             writeArray(r);
         }
-        static void forth7()
+        static void Forth7()
         //7.	Посчитать количество нечетных элементов массива
         {
             Console.WriteLine("задайте длину одномерного массива");
@@ -171,7 +179,7 @@ namespace HomeWork4
 
         }
 
-        static void forth8()
+        static void Forth8()
         //8.	Поменять местами первую и вторую половину массива, например, для массива 1 2 3 4, результат 3 4 1 2,  или для 12345 - 45312.
         {
             Console.WriteLine("задайте длину одномерного массива");
@@ -200,7 +208,7 @@ namespace HomeWork4
             writeArray(res);
         }
 
-        void forth9()
+        static void Forth9()
         //9.	Отсортировать массив по возрастанию одним из способов:  пузырьком(Bubble), выбором (Select) или вставками (Insert)) 
         {
             Console.WriteLine("задайте длину одномерного массива");
@@ -209,13 +217,70 @@ namespace HomeWork4
             int n = Int32.Parse(Console.ReadLine());
 
             int[] r = createArray(l, n);
-            int temp = r[0]; int t;
-            int[] res = new int[l];
+            int temp = r[0]; 
+            // int[] res = new int[l];
+            Console.WriteLine();
+            for (int i = 0; i < l; i++)
+            { for (int j = i+1; j < l; j++)
+                {
+                    if (r[i] >= r[j])
+                    {
+                        temp = r[i];
+                        r[i] = r[j];
+                        r[j] = temp;                        
+                    }
+                }
+            }
+            Console.WriteLine("отсортированный по возрастанию массив");
+            writeArray(r);
+            
+        }
+        //
+         static int maxIndex (int[] array)
+        //4.	Найти индекс максимального элемента массива
+        {     
+           int maxIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > array[maxIndex]) { maxIndex = i; }
+            }                  
+            return maxIndex;
+        }
 
+        static void Forth10()
+        //10.	Отсортировать массив по убыванию одним из способов, (отличным от способа в 9-м задании) :
+        //пузырьком(Bubble), выбором (Select) или вставками (Insert))
+        {
+            Console.WriteLine("задайте длину одномерного массива");
+            int l = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("задайте максимальное число диапазона значений массива");
+            int n = Int32.Parse(Console.ReadLine());
+
+            int[] r = createArray(l, n);
+            int temp;
+            int[] res = new int[l];
+            int mi = maxIndex(r);
+            
             for (int i = 0; i < l; i++)
             {
-
+                int max = r[i]; int maxIndex = i;
+                for (int j = i+1; j < l; j++)
+                {
+                    if (r[j] > r[maxIndex]) { maxIndex = j; }
+                } 
+                
+                if (i != maxIndex)
+                {
+                    temp = r[i];
+                    r[i] = r[maxIndex];
+                    r[maxIndex] = temp;
+                }
+                
             }
+            Console.WriteLine("отсортированный по убыванию массив");
+            writeArray(r);
+
+
         }
 
 
