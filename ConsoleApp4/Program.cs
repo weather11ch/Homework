80e9fdcaf21e46cf937923dcdcbeed60ea632bae
rchudaeva <weather11ch@gmail.com>
2024-01-23 14:59:47 +0300

    задание 6_1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,8 +8,30 @@ namespace ConsoleApp4
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Square f1 = new Square(5, "зеленый");
+            Square f2 = new Square(2, "синий");
+            Square f3 = new Square(15, "голубой");
+
+            List<Square> sqs = new List<Square>()
+            {
+                f1, f2, f3
+            };
+
+            foreach (var squares in sqs)
+            {
+                squares.AboutSqaure();
+            }
+            Square f4 = new Square(6, "зеленый");
+            sqs.Add(f4);
+            f1.a = 6;
+            foreach (var squares in sqs)
+            {
+                squares.AboutSqaure();
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ ConsoleApp4/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+public class Square
+{
+    public String col;
+    public int a;
+    public int s;
+    public Square(int a, String col)
+    {
+        this.a = a;
+        this.col = col;
+        s = a * a;
+    }
+    public virtual void AboutSqaure()
+    {
+        Console.WriteLine(col + " квадрат со стороной " + a + " и площадью " + s);
+    }
+}
